e822af4d9225f9188e43e7b47bc2e14046525e8a
Katherine Chartier <kchartier@mines.edu>
2023-11-07 10:14:10 -0700

    added lexing tests

--- src/lab5_tests.ml
+++ src/lab5_tests.ml
@@ -17,7 +17,36 @@ let lexer_tests =
      (Some("simple js"),
       "1 + 2",
       Ok([NUMBER(1.0); ADD_OP; NUMBER(2.0); EOF]));
-     (* TODO *)
+     (Some("simple js sub"),
+      "1 - 2",
+      Ok([NUMBER(1.0); SUB_OP; NUMBER(2.0); EOF]));
+      (Some("simple js mul"),
+      "1 * 2",
+      Ok([NUMBER(1.0); MUL_OP; NUMBER(2.0); EOF]));
+      (Some("simple js div"),
+      "1 / 2",
+      Ok([NUMBER(1.0); DIV_OP; NUMBER(2.0); EOF]));
+      (Some("simple equation"),
+      "1 + 1 = 2",
+      Ok([NUMBER(1.0); ADD_OP; NUMBER(1.0); ASSIGN_OP; NUMBER(2.0); EOF]));
+      (Some("simple equation 2"),
+      "2 * 3 < 9",
+      Ok([NUMBER(2.0); MUL_OP; NUMBER(3.0); LT_OP; NUMBER(9.0); EOF]));
+      (Some("complex equation"),
+      "(5 * 2) / (2 + 3) = 10",
+      Ok([LP_KW; NUMBER(5.0); MUL_OP; NUMBER(2.0); RP_KW; DIV_OP; LP_KW; NUMBER(2.0); ADD_OP; NUMBER(3.0); RP_KW; ASSIGN_OP; NUMBER(10.0); EOF]));
+      (Some("complex equation 2"),
+      "( 2 * ( 3 + 5)) / (( 2 * 6) / 3) <= 5",
+      Ok([LP_KW; NUMBER(2.0); MUL_OP; LP_KW; NUMBER(3.0); ADD_OP; NUMBER(5.0); RP_KW; RP_KW; DIV_OP; LP_KW; LP_KW; NUMBER(2.0); MUL_OP; NUMBER(6.0); RP_KW; DIV_OP; NUMBER(3.0); RP_KW; LEQ_OP; NUMBER(5.0); EOF]));
+      (Some("hello world"),
+      "Hello, world!",
+      Ok([IDENT("Hello"); COMMA_OP; IDENT("world"); LOG_NOT_OP; EOF]));
+      (Some("long string"),
+      "Hey! We are team BICK here doing Lab 5, please give us a good grade",
+      Ok([IDENT("Hey"); LOG_NOT_OP; IDENT("We"); IDENT("are"); IDENT("team"); IDENT("BICK"); IDENT("here"); IDENT("doing"); IDENT("Lab"); NUMBER(5.0); COMMA_OP; IDENT("please"); IDENT("give"); IDENT("us"); IDENT("a"); IDENT("good"); IDENT("grade"); EOF]));
+      (Some("line of code"),
+      "if x === 1 then y = 2;",
+      Ok([IDENT("if"); IDENT("x"); STREQ_OP; NUMBER(1.0); IDENT("then"); IDENT("y"); ASSIGN_OP; NUMBER(2.0); SEMICOLON_OP; EOF]));
 
    ])
 
